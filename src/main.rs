12ee1e95d159d43c04cd201a908541230561b6ad
Ivin Joel Abraham <ivinjabraham@gmail.com>
2025-06-29 10:07:32 +0530

    refactor: move dependecy check out of `run_app`
    The first step in `run_app` is to check if necessary external
    dependencies are installed. This does not need to be a method of the
    `App` struct nor does it need to part of `run_app`. Move dependecy check
    out of `run_app`.
    
    In the future, this check can be made by injecting any necessary
    configuration information instead of it being a method on the `App` struct.
    
    Signed-off-by: Ivin Joel Abraham <ivinjabraham@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,16 @@ mod lore;
 mod macros;
 mod ui;
 
-use std::ops::ControlFlow;
-
 use app::{config::Config, App};
 use clap::Parser;
 use cli::Cli;
+use color_eyre::eyre::bail;
 use handler::run_app;
 use infrastructure::{
     logging::Logger,
     terminal::{init, restore},
 };
+use std::ops::ControlFlow;
 
 fn main() -> color_eyre::Result<()> {
     let args = Cli::parse();
@@ -32,6 +32,10 @@ fn main() -> color_eyre::Result<()> {
     }
 
     let app = App::new(config)?;
+    if !app.check_external_deps() {
+        Logger::error("patch-hub cannot be executed because some dependencies are missing");
+        bail!("patch-hub cannot be executed because some dependencies are missing, check logs for more information");
+    }
 
     run_app(terminal, app)?;
     restore()?;
