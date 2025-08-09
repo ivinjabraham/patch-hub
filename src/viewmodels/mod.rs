87d08c580ce899bccc383caa9df0a8f94146c669
Ivin Joel Abraham <ivinjoelabraham@proton.me>
2025-08-09 12:46:31 +0530

    fix(ViewModel): change handle_key parameter to handle KeyEvent
    The previous implementation, `key_handling`, used to match `KeyEvent`
    and not `Event`. Correct the mistake in the new version.
    
    Additionally, remove the comments that were getting outdated quickly due
    to rapid development.
    
    Signed-off-by: Ivin Joel Abraham <ivinjoelabraham@proton.me>

--- src/viewmodels/mod.rs
+++ src/viewmodels/mod.rs
@@ -1,36 +1,11 @@
-use ratatui::crossterm::event::Event;
-#[allow(dead_code)]
-/// A trait representing a view model that can handle key events and expose its state.
-///
-/// # Associated Types
-/// - `State`: The type representing the state of the view model. Must have a `'static` lifetime.
-///
-/// # Required Methods
-/// - `handle_key`: Handles a key event, potentially mutating the view model.
-/// - `state`: Returns a reference to the current state.
-pub trait ViewModel {
-    /// Handles a key event, potentially mutating the view model.
-    ///
-    /// # Arguments
-    ///
-    /// * `event` - The event to handle.
-    fn handle_key(&mut self, event: Event);
+mod mailing_list_viewmodel;
 
-    /// Returns a reference to the current state.
+use ratatui::crossterm::event::KeyEvent;
+#[allow(dead_code)]
+pub trait ViewModel {
+    fn handle_key(&self, event: KeyEvent);
+
     fn state(&self) -> ViewModelState;
 }
 
-/// `ViewModelState` is an enum intended to represent the various possible
-/// state structs used by different screens in the application. Each variant
-/// of this enum should wrap the state struct corresponding to a specific screen,
-/// allowing for type-safe handling and dynamic dispatch of view model states.
-///
-/// Extend this enum by adding variants for each screen's state struct, for example:
-///
-/// ```
-/// enum ViewModelState {
-///     HomeScreen(HomeScreenState),
-///     SettingsScreen(SettingsScreenState),
-///     // Add more variants as needed
-/// }
 pub enum ViewModelState {}
